c75ba487f36080c2ec727d0343f02e9d1ae5d20b
Hyun Kong <konghyunsik7@gmail.com>
2022-07-14 10:19:03 -0400

    inital

---
+++ ClassesExercise/Car.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesExercise
+{
+    internal class Car
+    {
+        public Car()
+        {
+
+        }
+        public Car(string carMake, string carModel, int carYear, int carID)
+        {
+            Make = carMake;
+            Model = carModel;
+            Year = carYear;
+            ItemNum = carID;
+
+        }
+        public int ItemNum { get; set; }
+
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; } = 2022; //return a default value
+
+        public void Print()
+        {
+            Console.WriteLine($"{Make} | {Model} | {Year} | {ItemNum}");
+        }
+    }
+}
---
+++ ClassesExercise/Program.cs
@@ -0,0 +1,29 @@
+﻿using ClassesExercise;
+
+int carNum = 1;
+
+Car car1 = new Car();
+car1.Make = "Tesla";
+car1.Model = "Model S";
+car1.Year = 3000;
+car1.ItemNum = carNum++;
+
+var car2 = new Car();
+car2.Make = "Ford";
+car2.Model = "Focus";
+car2.Year = 2222;
+car2.ItemNum = carNum++;
+
+car1.Print();
+car2.Print();
+
+var joseCar = new Car()
+{
+    Make = "Bettle",
+    Model = "VW",
+    ItemNum = carNum++
+};
+joseCar.Print();
+
+var car3 = new Car();
+car3.Print();
